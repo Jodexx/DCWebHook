9192acf29cfa5a1bfe54ea2a00a2fe28ae686ebd
JodexIndustries <0984134q@gmail.com>
2023-09-12 16:42:13 +0300

    1.0.0

--- src/main/java/com/jodexindustries/dcwebhook/EventListener.java
+++ src/main/java/com/jodexindustries/dcwebhook/EventListener.java
@@ -1,2 +1,12 @@
-package com.jodexindustries.dcwebhook;public class EventListener {
+package com.jodexindustries.dcwebhook;
+
+import com.jodexindustries.donatecase.api.events.AnimationEndEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class EventListener implements Listener {
+    @EventHandler
+    public void onAnimationEnd(AnimationEndEvent e) {
+        // я усталь
+    }
 }
