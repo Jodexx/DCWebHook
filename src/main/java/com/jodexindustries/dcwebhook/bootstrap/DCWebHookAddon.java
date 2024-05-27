8efb9ab73adb2d682390e718efe9bfb687b6d490
Jodexx <0984134q@gmail.com>
2024-05-27 15:29:15 +0300

    api 2.2.1.7 adaptation

--- src/main/java/com/jodexindustries/dcwebhook/bootstrap/DCWebHookAddon.java
+++ src/main/java/com/jodexindustries/dcwebhook/bootstrap/DCWebHookAddon.java
@@ -3,13 +3,13 @@ package com.jodexindustries.dcwebhook.bootstrap;
 import com.jodexindustries.dcwebhook.tools.CustomConfig;
 import com.jodexindustries.dcwebhook.tools.Tools;
 import com.jodexindustries.donatecase.api.CaseManager;
-import com.jodexindustries.donatecase.api.addon.JavaAddon;
+import com.jodexindustries.donatecase.api.addon.internal.InternalJavaAddon;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.util.logging.Logger;
 
-public class DCWebHookAddon extends JavaAddon implements DCWebHook {
+public final class DCWebHookAddon extends InternalJavaAddon implements DCWebHook {
     public static CustomConfig customConfig;
     private CaseManager api;
     @Override
